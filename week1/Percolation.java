ddecab4f5c3fc44474439bebc64d924b37d2122f
Sergey Kambalin <greyexpert@gmail.com>
2015-06-24 12:36:00 +0600

    Classes skeletons

--- week1/Percolation.java
+++ week1/Percolation.java
@@ -1,9 +1,32 @@
-public class Percolation
+public class Percolation 
 {
-	public static void main( String[] args )
-	{
-	    QuickFindUF qf = new QuickFindUF(10);
-	
-		System.out.println("\n Grey \n");
-	}
+   public Percolation(int N)               // create N-by-N grid, with all sites blocked
+   {
+       
+   }
+   
+   public void open(int i, int j)          // open site (row i, column j) if it is not open already
+   {
+       
+   }
+   
+   public boolean isOpen(int i, int j)     // is site (row i, column j) open?
+   {
+       return true;
+   }
+   
+   public boolean isFull(int i, int j)     // is site (row i, column j) full?
+   {
+       return true;
+   }
+   
+   public boolean percolates()             // does the system percolate?
+   {
+       return true;
+   }
+
+   public static void main(String[] args)   // test client (optional)
+   {
+       
+   }
 }
